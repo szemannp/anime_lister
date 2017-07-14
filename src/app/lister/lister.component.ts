d9a316a4abbb07c728991043a9039486a57bec89
Peter Szemann <peter.szemann@blackswan.com>
2017-07-14 17:01:17 +0200

    lister component skeleton

---
+++ src/app/lister/lister.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lister',
+  templateUrl: './lister.component.html',
+  styleUrls: ['./lister.component.scss']
+})
+export class ListerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
